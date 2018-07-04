448ade805066ed6e5e3c2fa0f71707dcee3321ed
Sara S <ssantmier@gmail.com>
2018-07-04 17:21:26 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,3 @@
-color = 
-  R = "red"
-O = "orange"
-Y = "yellow"
-G = "green"
-B = "blue"
-I = "indigo"
-V = "violet"]
+color = [R = "red", O = "orange", Y = "yellow", G = "green", B = "blue", I = "indigo", V = "violet"]
   
-  
-  ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  Print 
